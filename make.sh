9cf227a4350b2e6c5f24b0cb4976ed8c926d096d
Mohammad Elmi <elmi.moh@gmail.com>
2015-03-02 09:16:49 +0330

    i3 config files added

--- make.sh
+++ make.sh
@@ -11,7 +11,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim i3"    # list of files/folders to symlink in homedir
 
 ##########
 
